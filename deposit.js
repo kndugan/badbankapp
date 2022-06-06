831fcc08c43a2e0384bdb77a0d8d2488f31b2dc6
Kristine Dugan <kndugan@gmail.com>
2022-06-06 11:36:52 -1000

    Update Balance Card

--- deposit.js
+++ deposit.js
@@ -1,4 +1,10 @@
 function Deposit(){
+  const [show, setShow]        = React.useState(true); 
+  const [status, setStatus]    = React.useState(''); 
+  const [balance, setBalance]  = React.useState('$100'); 
+  const [deposit, setDeposit]  = React.useState(''); 
+
+  const balanceMessage = "Balance: " + balance;
   const ctx = React.useContext(UserContext);
   return (
     <div className="container">
@@ -6,7 +12,7 @@ function Deposit(){
         bgcolor="light"
         txtcolor="black"
         header="Deposit"
-        body="Balance : " + {balance}
+        body={balanceMessage}
         />
     </div>
   )
