9ec85f10c2f4ca870a92ef6826d2094aa106724a
Kristine Dugan <kndugan@gmail.com>
2022-06-06 12:23:27 -1000

    Update home with User Name

--- home.js
+++ home.js
@@ -1,12 +1,20 @@
 function Home(){ 
+  const ctx = React.useContext(UserContext);
+  const name = ctx.users[0].name;
+  const email = ctx.users[0].email;
+
+  const welcomeMessage = "Welcome to the Bank " + name + "!";
+  const textMessage ="You can move around using the navigation bar.";
+  
+
   return (
     <div className="container">
       <Card 
         bgcolor="light"
         txtcolor="black"
         header="BadBank Landing Page"
-        title="Welcome to the Bank"
-        text="You can use this bank"
+        title={welcomeMessage}
+        text={textMessage}
         body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
       />  
     </div>
